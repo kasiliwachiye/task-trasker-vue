8101c2c2ca85b87b2b42c4969b3d0b868fc41f20
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2022-08-13 17:41:38 +0300

    chore: add routing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,40 @@
 <template>
-  <div>
-    <header>
-      <h1>{{ title }}</h1>
-      <ButtonComponent
-        :title="showAddTask ? 'Close' : 'Add Task'"
-        :color="showAddTask ? 'red' : 'green'"
-        @btn-click="$emit('toggle-add-task')"
-      />
-    </header>
-  </div>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      v-show="homePage"
+      @btn-click="$emit('toggle-add-task')"
+      :title="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
 </template>
 
 <script>
-import ButtonComponent from "./Button.vue";
+import Button from './Button'
 
 export default {
-  name: "HeaderComponent",
+  name: 'HeaderComponent',
   props: {
     title: String,
     showAddTask: Boolean,
   },
   components: {
-    ButtonComponent,
+    Button,
   },
-};
+  computed: {
+    homePage() {
+      if (this.$route.path === '/') {
+        return true
+      } else {
+        return false
+      }
+    },
+  },
+}
 </script>
 
-<style>
+<style scoped>
 header {
   display: flex;
   justify-content: space-between;
---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>Version 1.0.0</p>
+  <router-link to="/">Go Back</router-link>
+</template>
+
+<script>
+export default {
+  name: "AboutComponent",
+};
+</script>
